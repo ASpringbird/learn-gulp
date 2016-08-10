ea4d9c98d5dc6d366675acfdf686e2a7a47a6cc8
yangbo <yangbo@tigerbrokers.com>
2016-08-10 18:14:24 +0800

    更加完善的css任务
    包括编译，格式化，前缀，压缩等

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+
+// css 相关插件
+var sass         = require('gulp-sass');         // 编译sass
+var autoprefixer = require('gulp-autoprefixer'); // 补全前缀
+var csscomb      = require('gulp-csscomb');      // 格式化css样式，排序
+var cssbeautify  = require('gulp-cssbeautify');  // 美化css样式
+var minifycss    = require('gulp-minify-css');   // 压缩css文件
+
+
+// 错误捕获
+var plumber = require('gulp-plumber');
 
 gulp.task('css', function() {
-	return gulp.src('./style/*.scss')
+	gulp.src('./style/*.scss')
+		.pipe(plumber())
 		.pipe(sass())
-		.pipe(gulp.dest('./style/'))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'Android >= 4.0'],
+			cascade: true, //是否美化属性值 默认：true 像这样：
+                           //-webkit-transform: rotate(45deg);
+                           //        transform: rotate(45deg);
+            remove:true //是否去掉不必要的前缀 默认：true
+		}))
+		.pipe(csscomb())
+		.pipe(minifycss({
+			aggressiveMerging: false,
+            advanced: false,            //类型：Boolean 默认：true [是否开启高级优化（合并选择器等）]
+            compatibility: 'ie7',       //保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
+            keepBreaks: true,           //类型：Boolean 默认：false [是否保留换行]
+			keepSpecialComments: '*'    //保留所有特殊前缀 当你用autoprefixer生成的浏览器前缀，如果不加这个参数，有可能将会删除你的部分前缀
+		}))
+		.pipe(gulp.dest('./style/'));
 });
